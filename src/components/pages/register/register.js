8d2198063233341acaee21bfebdf602e017bd0c8
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-05-30 00:36:13 +0530

    by_sachin

--- src/components/pages/register/register.js
+++ src/components/pages/register/register.js
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 import styles from "./register.module.css";
-
+import { Login } from "../../organisms/login/login";
 
 export function SignInUp(){
    return (
       <Fragment>
          <div className={styles.mainbox}>
 
-          
+          <Login/>
 
 
             {/* <div className={styles.login}>
