092d318ce6e558d45424819f71c77077b18c39d2
ManishKuPatra254 <manishpatra254@gmail.com>
2023-05-30 00:32:45 +0530

    added the create account part in group number 3

--- src/components/pages/register/register.js
+++ src/components/pages/register/register.js
@@ -1,29 +1,30 @@
 import { Fragment } from "react";
 import styles from "./register.module.css";
+import { CreateAccOfTwitter } from "../../organisms/CreateAcc/createAccount";
 
 
-export function SignInUp(){
+
+export function SignInUp() {
    return (
       <Fragment>
          <div className={styles.mainbox}>
 
-          
 
+            {/* min-width : 280px 
+            widith : 280px
+               height : 90%  */}
 
             {/* <div className={styles.login}>
 
-         
-              
-
-
-
             </div> */}
-            
+
+            <CreateAccOfTwitter />
 
 
 
 
-          </div>
+
+         </div>
 
 
       </Fragment>
