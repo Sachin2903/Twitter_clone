e06f7cdf91032fb1dcfe9b50eb9b442dd8cc14c3
Abhay <abhaykhemnar07@gmail.com>
2023-05-31 00:40:46 +0530

    leftSideComponent by Abhay

--- src/components/organisms/main/leftSideBar/leftSideBar.js
+++ src/components/organisms/main/leftSideBar/leftSideBar.js
@@ -15,20 +15,20 @@ export function LeftSideBar(){
         <Fragment>
             <div className={styles.leftSideBarbox}>
               <img src={logo} className={styles.twitterLogo} alt="Loading"/>
-              <button class={styles.button}> <p><ImHome class={styles.logo}/> Home  </p> </button>
-              <button class={styles.button}> <p><BiHash class={styles.logo}/> Explore  </p> </button>
-              <button class={styles.button}> <p><GrNotification class={styles.logo}/> Notification  </p> </button>
-              <button class={styles.button}> <p><BiEnvelope class={styles.logo}/> Messages  </p> </button>
-              <button class={styles.button}> <p><RiFileListLine class={styles.logo}/> List  </p> </button>
-              <button class={styles.button}> <p><BiBookmark class={styles.logo}/> Bookmark  </p> </button>
-              <button class={styles.button}> <p><CgProfile class={styles.logo}/> Profile  </p> </button>
-              <button class={styles.button}> <p><CiCircleMore class={styles.logo}/> More  </p> </button>
+              <button className={styles.button}> <p><ImHome className={styles.logo}/> Home  </p> </button>
+              <button className={styles.button}> <p><BiHash className={styles.logo}/> Explore  </p> </button>
+              <button className={styles.button}> <p><GrNotification className={styles.logo}/> Notification  </p> </button>
+              <button className={styles.button}> <p><BiEnvelope className={styles.logo}/> Messages  </p> </button>
+              <button className={styles.button}> <p><RiFileListLine className={styles.logo}/> List  </p> </button>
+              <button className={styles.button}> <p><BiBookmark className={styles.logo}/> Bookmark  </p> </button>
+              <button className={styles.button}> <p><CgProfile className={styles.logo}/> Profile  </p> </button>
+              <button className={styles.button}> <p><CiCircleMore className={styles.logo}/> More  </p> </button>
 
            
-              <button class={styles.tweet}>Tweet</button>
-              <div class={styles.profile}>
-                 <img class={styles.imge} src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=360&t=st=1685463036~exp=1685463636~hmac=2d5357a2e8f08296abd57f7778b113ef2de9e19b5f273c64e99cc0334cab2ba3" alt="avater"/> 
-                 <p> <h3>Tony Stark</h3>  @tony stark </p> 
+              <button className={styles.tweet}>Tweet</button>
+              <div className={styles.profile}>
+                 <img className={styles.imge} src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=360&t=st=1685463036~exp=1685463636~hmac=2d5357a2e8f08296abd57f7778b113ef2de9e19b5f273c64e99cc0334cab2ba3" alt="avater"/> 
+                 <div> <h3>Tony Stark</h3>  @tony stark </div> 
               </div>
             </div>
             
