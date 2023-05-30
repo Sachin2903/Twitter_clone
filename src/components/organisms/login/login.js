8e36499fceb5b56e999653db831af1602cb9233a
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-05-30 11:43:11 +0530

    add more changes by sachin

--- src/components/organisms/login/login.js
+++ src/components/organisms/login/login.js
@@ -6,11 +6,36 @@ import { BsApple } from "react-icons/bs";
 import { RxCross2 } from "react-icons/rx";
 import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 export function Login() {
+    const inputbox = useRef("");
+    const inputboxalert = useRef("");
+
+    
+        const userDetailsToMatch=localStorage.getItem("userDetail");
+        
+   
+
+
+    function checkLoginField() {
+        if (inputbox.current.value.trim().length <= 0) {
+            inputboxalert.current.style.display = "block";
+            setInterval(() => {
+                inputboxalert.current.style.display = "none";
+            }, 4000)
+
+        }else{
+           
+        }
+
+
+    }
+
     return (
         <Fragment>
 
             <div className={styles.loginbox}>
+                <p ref={inputboxalert} className={styles.alertbox}>Sorry, we could not find your account.</p>
                 <RxCross2 className={styles.crossbtn} />
                 <BsTwitter className={styles.logotwitter} />
                 <h2 className={styles.headtext}>Sign in to Twitter</h2>
@@ -19,14 +44,17 @@ export function Login() {
                 <p className={styles.ortext}> or </p>
 
                 <TextField
-                    sx={{ width: "50%" ,margin:"20px 0px" }}
+                    inputRef={inputbox}
+
+                    sx={{ width: "50%", margin: "20px 0px" }}
                     label="Phone, email, or username "
                     multiline
 
-                />
-               
 
-                <button className={styles.btnnext}>Next</button>
+                />
+
+
+                <button onClick={checkLoginField} className={styles.btnnext}>Next</button>
                 <button className={styles.btnapple}>Forgot password?</button>
                 <p className={styles.headtext}>Don't have an account?<span style={{ color: "rgb(27, 169, 225)", cursor: "pointer" }}>
                     <Link className={styles.signuptext} to="/signuppage">Sign up</Link></span></p>
