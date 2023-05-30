20bf02233eccf1d116363c69298751df14192f25
szakeerfu <shaikzakeer202557@gmail.com>
2023-05-31 00:28:07 +0530

    zakeer search bar

--- src/components/organisms/main/rightSideBar/rightSideBar.js
+++ src/components/organisms/main/rightSideBar/rightSideBar.js
@@ -1,15 +1,45 @@
 import { Fragment } from "react"
 import styles from "./rightSideBar.module.css";
+import { AiOutlineSearch } from 'react-icons/ai'
 
 
 
-export function RightSideBar(){
+
+export function RightSideBar() {
     return (
         <Fragment>
-            <div className={styles.rightSideBarbox}>
+            <div className={styles.sidebar}>
+                <div className={styles.inline}>
+                    <div className={styles.input_icons}>
+                        <AiOutlineSearch className={styles.search} />
+
+                        <input className={styles.input_field} type="text"
+                        />
+                        <div className={styles.sidebarcontainer}>
+                            <h1 className={styles.heading}>
+                                What's happening?
+                            </h1>
+                        </div>
+                        <div className={styles.follow}>
+
+                            <h1>
+                                Who To Follow
+                            </h1>
+
+                        </div>
+
+                    </div>
+
+
+
+                </div>
 
             </div>
-            
+
+
+
+
+
         </Fragment>
 
     )
