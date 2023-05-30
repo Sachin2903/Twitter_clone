963482ae65104c7896c04bc6518e05b62345a0e4
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-05-30 14:33:10 +0530

    add main loader by sachin

--- src/components/pages/register/register.js
+++ src/components/pages/register/register.js
@@ -1,16 +1,35 @@
-import { Fragment } from "react";
+import { Fragment ,useLayoutEffect,useRef} from "react";
 import styles from "./register.module.css";
- import {Login} from "../../organisms/login/login"
-
+ import {Login} from "../../organisms/login/login";
+ import logo from "../../../image/logo/logo.png"
 
 export function SignInUp(){
+   const loaderingscreen=useRef("");
+
+   useLayoutEffect(()=>{
+      loaderingscreen.current.style.display="block";
+       let idmainloader=setTimeout(()=>{
+         loaderingscreen.current.style.display="none";
+
+       },2000)
+    return ()=>{
+      clearTimeout(idmainloader);
+    }
+
+   },[])
+
+
+
    return (
       <Fragment>
          <div className={styles.mainbox}>
         <Login/>
        
           </div>
+           <div ref={loaderingscreen} className={styles.mainloading}>
+            <img alt="loading.." className={styles.twitterlogo} src={logo}/>
 
+           </div>
 
       </Fragment>
 
