3138575878a9db07c09d2d4886b0eccbbf6f3d99
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-01 20:52:07 +0530

    sachin

---
+++ src/slice/managetweetData/managetweetdata.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+export const tweetSlice = createSlice({
+    initialState: [],
+    name: "tweetnow",
+    reducers: {
+       
+    }
+})
--- src/store/manageHomeDataStore.js
+++ src/store/manageHomeDataStore.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import {whatHappenSlice} from "../slice/manageWhatHappenData";
-
+import {tweetSlice} from "../slice/managetweetData/managetweetdata"
 
 export const store=configureStore({
     reducer:{
         whatHappen:whatHappenSlice.reducer,
+        tweetnow:tweetSlice.reducer
         
         
     }
