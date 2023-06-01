6ce1fa2986218058376e0d1208c25c8f2bca48be
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-01 21:54:19 +0530

    done by sachin

--- src/slice/managetweetData/managetweetdata.js
+++ src/slice/managetweetData/managetweetdata.js
@@ -1,11 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import {tweetdummydata} from "./managetweetdummydata";
 
 
 export const tweetSlice = createSlice({
-    initialState: [],
+    initialState: [...tweetdummydata],
     name: "tweetnow",
     reducers: {
+        addlike:((state,action)=>{
+            state.map((e,i)=>{
+                if(action.payload===i){
+                    if(e.liketrue==="true"){
+                        e.likesCount=e.likesCount-1;
+                        e.liketrue="false";
+                    }else{
+                        e.likesCount=e.likesCount+1
+                        e.liketrue="true";
+                    }
+                }
+
+            })
+        }),
+
+
+
+
        
     }
 })
