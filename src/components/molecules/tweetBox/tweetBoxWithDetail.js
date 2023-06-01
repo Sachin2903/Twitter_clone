ffcbafb2b24c0baa44737c035692d3143ef4a3db
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-01 13:07:19 +0530

    add a tweetBox by Sachin

---
+++ src/components/molecules/tweetBox/tweetBoxWithDetail.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import styles from "./tweetBoxWithDetail.module.css";
+export function TweetBoxWithDetail(){
+    return(
+        <Fragment>
+            <div>
+
+            </div>
+        </Fragment>
+    )
+}
