d26f7c3bba30e1845f22fe1793ff5072453d5a3c
szakeerfu <shaikzakeer202557@gmail.com>
2023-06-01 16:42:32 +0530

    side bar by zakeer

--- src/components/molecules/RightSideBarMore/whatsHappen/WhatsHapp.js
+++ src/components/molecules/RightSideBarMore/whatsHappen/WhatsHapp.js
@@ -2,41 +2,39 @@ import React from "react";
 import { Fragment, useState } from "react";
 import styles from "./whatsHappen.module.css";
 import { HiEmojiSad } from "react-icons/hi";
-import {whatHappenSlice} from "../../../../slice/manageWhatHappenData"
+import { whatHappenSlice } from "../../../../slice/manageWhatHappenData";
 
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export const WhatsHapp = () => {
- const dispatchFromWhatHappen=useDispatch();
+  const dispatchFromWhatHappen = useDispatch();
 
-  const detailsOnRightSide =useSelector((state,action)=>{
-    return state.whatHappen
-  })
+  const detailsOnRightSide = useSelector((state, action) => {
+    return state.whatHappen;
+  });
 
+  function deleteWhatHappen(id) {
+    dispatchFromWhatHappen(whatHappenSlice.actions.whatHappenDelete(id));
+  }
 
-
-function deleteWhatHappen(id){
-  dispatchFromWhatHappen(whatHappenSlice.actions.whatHappenDelete(id))
-  
-}
-
-function addOnSeeMore(){
-  dispatchFromWhatHappen(whatHappenSlice.actions.whatHappenSeeMore())
-
-}
-
-  const [showPopupIndex, setShowPopupIndex] = useState(-1);
-
+  function addOnSeeMore() {
+    dispatchFromWhatHappen(whatHappenSlice.actions.whatHappenSeeMore());
+  }
+  const [showPopup, setShowPopup] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(-1);
   const togglePopup = (index) => {
-    setShowPopupIndex(showPopupIndex === index ? -1 : index);
+    if (activeIndex === index) {
+      setShowPopup(false);
+      setActiveIndex(-1);
+    } else {
+      setShowPopup(true);
+      setActiveIndex(index);
+    }
   };
-
   return (
     <Fragment>
       <div className={styles.treanding_main}>
-        {
-        detailsOnRightSide.map((data, index) => (
-
+        {detailsOnRightSide.map((data, index) => (
           <div className={styles.data_trend} key={index}>
             <p>{data.whatHappenTitle}</p>
             <div className={styles.dots}>
@@ -46,26 +44,29 @@ function addOnSeeMore(){
               >
                 ...
               </button>
-              {showPopupIndex === index && (
-                <div className={styles.popup}>
-                  <HiEmojiSad style={{ fontSize: "25px" ,marginTop:"10px"}} />
+              {showPopup && activeIndex === index && (
+                <div
+                  className={styles.popup}
+                  onClick={() => togglePopup(index)}
+                >
+                  <HiEmojiSad style={{ fontSize: "25px", marginTop: "10px" }} />
                   <h4
                     onClick={() => deleteWhatHappen(index)}
                     className={styles.popupdata}
                   >
                     Not interested in this
                   </h4>
-                  <br></br>
+                  <br />
                 </div>
               )}
             </div>
             <h3>Topic: {data.whatHappenTopic}</h3>
             <h4>MetaData: {data.whatHappenMetaData}</h4>
           </div>
-              
-        ))
-        }
-        <button onClick={addOnSeeMore} className={styles.show_more}>Show more</button>
+        ))}
+        <button onClick={addOnSeeMore} className={styles.show_more}>
+          Show more
+        </button>
       </div>
     </Fragment>
   );
