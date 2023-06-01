14da16b280d1cd65ad93595a5ac3d693964e5f79
ManishKuPatra254 <manishpatra254@gmail.com>
2023-06-01 12:13:04 +0530

    added by manish

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 
-import {Sign} from "./components/organisms/signup/Sign"
+import { Sign } from "./components/organisms/signup/Sign"
 
-import {CreateAccOfTwitter} from "./components/organisms/CreateAcc/createAccount";
+import { CreateAccOfTwitter } from "./components/organisms/CreateAcc/createAccount";
 import { Home } from "./components/pages/home/home";
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
-import {SignInUp} from "./components/pages/register/register";
+import { Routes, Route } from 'react-router-dom';
+import { SignInUp } from "./components/pages/register/register";
 import { Page404 } from "./components/pages/page404/page404";
-import {DummyHome} from "./components/pages/dummyHomePage/dummyHome"
-import { TweetBox } from "./components/organisms/main/leftSideBar/tweetBox/tweetbox";
+import { DummyHome } from "./components/pages/dummyHomePage/dummyHome"
+
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<SignInUp/>}/>
-        <Route path="/signuppage" element={<Sign/>}/>
-        <Route path="/createaccount" element={<CreateAccOfTwitter/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/dummyHome" element={<DummyHome/>}/>
-         <Route path="*" element={<Page404/>}/>
-         <Route path="/tweetbox" element={<TweetBox/>}/>
-      
-      
+      <Route path="/" element={<SignInUp />} />
+      <Route path="/signuppage" element={<Sign />} />
+      <Route path="/createaccount" element={<CreateAccOfTwitter />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/dummyHome" element={<DummyHome />} />
+      <Route path="*" element={<Page404 />} />
+
+
+
 
     </Routes>
   );
